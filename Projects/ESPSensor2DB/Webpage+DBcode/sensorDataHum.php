d5e6f0a3d15033d8b9ce8dca59897c62b1deca2d
KYLEREXLEY <89437044+KYLEREXLEY@users.noreply.github.com>
2025-10-23 01:47:37 -0700

    Create sensorDataHum.php

---
+++ Projects/ESPSensor2DB/Webpage+DBcode/sensorDataHum.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+$dbHost = "localhost";
+$dbName = "name";         
+$dbUser = "user";       
+$dbPass = "password";
+
+try{
+  $pdo=new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4",$dbUser,$dbPass,[
+    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC
+  ]);
+  $stmt=$pdo->query("
+    SELECT DATE_FORMAT(time_received,'%Y-%m-%d %H:%i:%s') AS time_received, humidity
+    FROM sensor_data WHERE node_name='node_1' ORDER BY time_received ASC
+  ");
+  echo json_encode($stmt->fetchAll());
+}catch(Throwable $e){
+  http_response_code(500);
+  echo json_encode(['error'=>$e->getMessage()]);
+}
